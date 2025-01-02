9eefb32bc725236b09fbf4cac33b7b56fc882e3e
navneetshukl <navneetshukla824@gmail.com>
2025-01-02 16:52:34 +0530

    code to upload pdf

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 )
 
 require (
+	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.7 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.22 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.26 // indirect
@@ -25,5 +26,16 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.28.7 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.33.3 // indirect
 	github.com/aws/smithy-go v1.22.1 // indirect
+	github.com/gofiber/fiber/v2 v2.52.6 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/ledongthuc/pdf v0.0.0-20240201131950-da5b75280b06 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
 )
---
+++ internal/interface/api/handler/handler.go
@@ -0,0 +1,65 @@
+package handler
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Handler struct {
+}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+func (h *Handler) UploadFile(c *fiber.Ctx) error {
+	file, err := c.FormFile("file")
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Failed to upload file",
+		})
+	}
+
+	// Check if the file size exceeds 10 MB (10 * 1024 * 1024 bytes)
+	const maxFileSize = 10 * 1024 * 1024 // 10 MB
+	if file.Size > maxFileSize {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "File size exceeds the 10 MB limit",
+		})
+	}
+
+	// Ensure the uploaded file is a PDF
+	if filepath.Ext(file.Filename) != ".pdf" {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Only PDF files are allowed",
+		})
+	}
+
+	// Define the destination path
+	savePath := fmt.Sprintf("./uploads/%s", file.Filename)
+
+	// Create the uploads directory if it doesn't exist
+	if _, err := os.Stat("./uploads"); os.IsNotExist(err) {
+		if err := os.Mkdir("./uploads", os.ModePerm); err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Failed to create upload directory",
+			})
+		}
+	}
+
+	// Save the file
+	if err := c.SaveFile(file, savePath); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to save file",
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "File uploaded successfully",
+		"path":    savePath,
+	})
+
+}
