d6b70af70b7b1542fd72c5c1a0961aaddb1347be
navneetshukl <navneetshukla824@gmail.com>
2025-01-04 21:30:37 +0530

    routes

--- internal/interface/api/routes.go
+++ internal/interface/api/routes.go
@@ -1,1 +1,19 @@
 package routes
+
+import (
+	"file-modification/internal/interface/api/handler"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func SetupRoutes(h *handler.Handler) *fiber.App {
+
+	routes:=fiber.New()
+
+	routes.Post("/upload", h.UploadFile)
+
+	return routes
+
+
+
+}
